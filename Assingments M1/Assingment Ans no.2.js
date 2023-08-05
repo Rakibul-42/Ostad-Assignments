4ab5a55edd8d37feca10183a9fc615f7c16ccc97
Rakibul Ansary <140431602+Rakibul-42@users.noreply.github.com>
2023-08-05 20:50:01 +0600

    Delete Assingments M1 directory

--- Assingments M1/Assingment Ans no.2.js
+++
@@ -1,9 +0,0 @@
-function filterEvenNumbers(arr) {
-    return arr.filter(num => num % 2 === 0);
-  }
-  
-  const originalArray = [11, 36, 44, 23, 1066, 72, 54, 98];
-  const filteredEvenNumbers = filterEvenNumbers(originalArray);
-  
-  console.log("Original Array: " + (originalArray));
-  console.log("Filtered Even Numbers: " + (filteredEvenNumbers));
