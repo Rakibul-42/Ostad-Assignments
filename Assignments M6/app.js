55b42dc8f851d6368a817483deb3c2d3f5a69a86
Rakibul Ansary <140431602+Rakibul-42@users.noreply.github.com>
2023-09-12 20:49:37 +0600

    Delete Assignments M6 directory

--- Assignments M6/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const apiRouter = require('./src/Routes/api');
-const port = 3000;
-
-
-app.use('/src/Routes/api', apiRouter);
-module.exports = app;
