1e9a639a0d80104de840adb7031988c4a5b69fd0
Rakibul Ansary <140431602+Rakibul-42@users.noreply.github.com>
2023-10-01 23:06:18 +0600

    Delete Assignment M7 directory

--- Assignment M7/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app');
-const port = process.env.RUNNING_PORT || 5500;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
--- Assignment M7/readme.md
+++
@@ -1,1 +0,0 @@
-config.env and NM wasn't supposed to be uploaded. But i uploaded the config because it's also a part of the assignment by the instructions given. Thank you .
