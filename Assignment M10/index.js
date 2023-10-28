3936ed774ae36b6b379a4d29f64c937775f2e5dd
Rakibul Ansary <140431602+Rakibul-42@users.noreply.github.com>
2023-10-29 00:24:31 +0600

    Delete Assignment M10 directory

--- Assignment M10/index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-const dotenv = require("dotenv");
-dotenv.config({path: "./config.env"});
-
-const port = process.env.RUNNING_PORT;
-
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
-});
