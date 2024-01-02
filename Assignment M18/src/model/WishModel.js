d56532560e3d6ecf37dfe9608500e4e478acb734
Rakibul Ansary <140431602+Rakibul-42@users.noreply.github.com>
2024-01-02 20:14:58 +0600

    Delete Assignment M18 directory

--- Assignment M18/src/model/WishModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose') ;
-
-const DataSchema =  mongoose.Schema({
-    ProductID:{type:mongoose.Schema.Types.ObjectId , required:true},
-    userID:{type:mongoose.Schema.Types.ObjectId , required:true},
-} , {timestamp :true , versionKey : false});
-
-const WishModel = mongoose.model('Wishes' , DataSchema);
-module.exports = WishModel
